a80af682d66cb9ab9f5e9da06d2405b95e61bcbc
zhouyanqiang <zhouyanqiang@onething.net>
2019-09-11 20:31:29 +0800

    [add] regression CART

---
+++ Decision Tree/Regression Tree.py
@@ -0,0 +1,40 @@
+'''
+Reference:
+[1] https://blog.csdn.net/weixin_40604987/article/details/79296427
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import linear_model
+
+# Data set
+x = np.array(list(range(1, 11))).reshape(-1, 1)
+y = np.array([5.56, 5.70, 5.91, 6.40, 6.80, 7.05, 8.90, 8.70, 9.00, 9.05]).ravel()
+
+# Fit regression model
+model1 = DecisionTreeRegressor(max_depth=1)
+model2 = DecisionTreeRegressor(max_depth=3)
+model3 = linear_model.LinearRegression()
+model1.fit(x, y)
+model2.fit(x, y)
+model3.fit(x, y)
+
+# Predict
+X_test = np.arange(0.0, 10.0, 0.01)[:, np.newaxis]
+y_1 = model1.predict(X_test)
+y_2 = model2.predict(X_test)
+y_3 = model3.predict(X_test)
+
+# Plot the results
+plt.figure()
+plt.scatter(x, y, s=20, edgecolor="black",
+            c="darkorange", label="data")
+plt.plot(X_test, y_1, color="cornflowerblue",
+         label="max_depth=1", linewidth=2)
+plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=3", linewidth=2)
+plt.plot(X_test, y_3, color='red', label='liner regression', linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
